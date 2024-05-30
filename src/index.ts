d6b14dcdf3340fc50f42b8909c16e8de5d81373c
Nick Thomas <nichoth@nichoth.com>
2024-05-30 11:08:48 -0700

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,40 @@
+#!/usr/bin/env node
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+import sharp from 'sharp'
+import { fileTypeFromFile } from 'file-type'
+
 export function example ():void {
     console.log('hello')
+}
+
+const args = yargs(hideBin(process.argv))
+    .demandCommand(1)
+    .command('filename', 'the local filename to read')
+    .option('size', {
+        alias: 's',
+        type: 'number',
+        description: 'The width of the base64 image; default is 20'
+    })
+    .example('`npx stringify my-fiile.jpg`',
+        'Create a small base64 string from a local file')
+    .usage('Usage: stringify <filename>')
+    .argv
+
+const filename = args._[0]
+const { size } = args
+const uri = await getImgFromFile(filename, size)
+process.stdout.write(uri + '\n')
+
+async function getImgFromFile (filename, size = 20) {
+    const buf = await sharp(filename)
+        .resize(size)
+        .jpeg()
+        .toBuffer()
+
+    const type = await fileTypeFromFile(filename)
+    const { mime } = type!
+    const base64 = buf.toString('base64')
+    const dataUri = 'data:' + mime + ';base64,' + base64
+    return dataUri
 }
